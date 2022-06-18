10944ce5ab93a5f650d9d9acc806e0bb959245f6
Michael Caley <mjcaley@darkarctic.com>
2022-06-17 21:48:23 -0400

    Added Tok utility parser to simplify whitespace

--- CloudQL.Parser/QLParser.cs
+++ CloudQL.Parser/QLParser.cs
@@ -104,27 +104,28 @@ namespace CloudQL.QLParser
 
     public static class QueryLanguage
     {
+        private static Parser<char, T> Tok<T>(Parser<char, T> token) => Try(token).Before(SkipWhitespaces);
+        private static Parser<char, string> Tok(string token) => Tok(String(token));
 
-        public static readonly Parser<char, Keywords> Select = String("select").ThenReturn(Keywords.Select);
-        public static readonly Parser<char, Keywords> From = String("from").ThenReturn(Keywords.From);
-        public static readonly Parser<char, BooleanOperator> And = String("and").ThenReturn(BooleanOperator.And);
-        public static readonly Parser<char, BooleanOperator> Or = String("or").ThenReturn(BooleanOperator.Or);
-        public static readonly Parser<char, UnaryOperator> Not = String("not").ThenReturn(UnaryOperator.Not);
-        public static readonly Parser<char, ComparisonOperator> Equal = from _ in String("==")
+        public static readonly Parser<char, Keywords> From = Tok("from").ThenReturn(Keywords.From);
+        public static readonly Parser<char, BooleanOperator> And = Tok("and").ThenReturn(BooleanOperator.And);
+        public static readonly Parser<char, BooleanOperator> Or = Tok("or").ThenReturn(BooleanOperator.Or);
+        public static readonly Parser<char, UnaryOperator> Not = Tok("not").ThenReturn(UnaryOperator.Not);
+        public static readonly Parser<char, ComparisonOperator> Equal = from _ in Tok("==")
                                                                         select ComparisonOperator.Equal;
-        public static readonly Parser<char, ComparisonOperator> NotEqual = from _ in String("!=")
+        public static readonly Parser<char, ComparisonOperator> NotEqual = from _ in Tok("!=")
                                                                            select ComparisonOperator.NotEqual;
-        public static readonly Parser<char, ComparisonOperator> LessThan = from _ in String("<")
+        public static readonly Parser<char, ComparisonOperator> LessThan = from _ in Tok("<")
                                                                            select ComparisonOperator.LessThan;
-        public static readonly Parser<char, ComparisonOperator> GreaterThan = from _ in String(">")
+        public static readonly Parser<char, ComparisonOperator> GreaterThan = from _ in Tok(">")
                                                                               select ComparisonOperator.GreaterThan;
-        public static readonly Parser<char, ComparisonOperator> LessThanOrEqual = from _ in String("<=")
+        public static readonly Parser<char, ComparisonOperator> LessThanOrEqual = from _ in Tok("<=")
                                                                                   select ComparisonOperator.LessThanOrEqual;
-        public static readonly Parser<char, ComparisonOperator> GreaterThanOrEqual = from _ in String(">=")
+        public static readonly Parser<char, ComparisonOperator> GreaterThanOrEqual = from _ in Tok(">=")
                                                                                      select ComparisonOperator.GreaterThanOrEqual;
          
-        public static readonly Parser<char, Operators> Dot = Char('.').ThenReturn(Operators.Dot);
-        public static readonly Parser<char, Operators> Comma = Char(',').ThenReturn(Operators.Comma);
+        public static readonly Parser<char, Operators> Dot = Tok(".").ThenReturn(Operators.Dot);
+        public static readonly Parser<char, Operators> Comma = Tok(",").ThenReturn(Operators.Comma);
 
         public static readonly Parser<char, string> Identifier = from first in Letter
                                                                  from rest in OneOf(LetterOrDigit, Char('_')).ManyString()
@@ -134,25 +135,44 @@ namespace CloudQL.QLParser
                                                                  from child in Rec(() => Dot.Then(Resource)).Optional()
                                                                  select new Resource() { Name = ident, Child = child.GetValueOrDefault(() => null) };
 
-        public static readonly Parser<char, Expression> IdentifierAtom = from ident in Identifier
+        public static readonly Parser<char, Expression> IdentifierAtom = from ident in Tok(Identifier)
                                                                          select new IdentifierExpression() { Identifier = ident } as Expression;
-        public static readonly Parser<char, Expression> IntegerAtom = from integer in LongNum
+        public static readonly Parser<char, Expression> IntegerAtom = from integer in Tok(LongNum)
                                                                       select new IntegerExpression() { Integer = integer } as Expression;
-        public static readonly Parser<char, Expression> FloatAtom = from floatAtom in Real
+
+        private static readonly Parser<char, string> FloatFirst = OneOf(
+            String("0"),
+            Digit.Where(c => c != '0').Then(Digit.ManyString())
+            );
+        public static readonly Parser<char, double> FloatLiteral = FloatFirst.Then(Char('.')).Then(Digit.Many()).MapWithInput(
+            (span, _) =>
+            {
+                var success = double.TryParse(span, out var result);
+                if (success)
+                {
+                    return (double?)result;
+                }
+                return null;
+            }
+            )
+            .Assert(result => result.HasValue)
+            .Select(result => result!.Value);
+                                                                   
+        public static readonly Parser<char, Expression> FloatAtom = from floatAtom in Tok(FloatLiteral)
                                                                     select new FloatExpression() { Float = floatAtom } as Expression;
         public static readonly Parser<char, Expression> Atom = OneOf(IdentifierAtom, Try(FloatAtom), Try(IntegerAtom));
         public static readonly Parser<char, Expression> Unary = OneOf(
                                                                     Try(
                                                                         from notOp in Not
-                                                                        from expr in SkipWhitespaces.Then(Expression)
+                                                                        from expr in Tok(Expression)
                                                                         select new UnaryExpression() { Operator = notOp, Right = expr } as Expression
                                                                     ),
                                                                     Atom);
         public static readonly Parser<char, Expression> Comparison = OneOf(
                                                                         Try(
                                                                             from leftExpr in Unary
-                                                                            from compOp in SkipWhitespaces.Then(OneOf(Try(Equal), Try(NotEqual), Try(LessThanOrEqual), Try(GreaterThanOrEqual), Try(LessThan), Try(GreaterThan)))
-                                                                            from rightExpr in SkipWhitespaces.Then(Expression)
+                                                                            from compOp in OneOf(Try(Equal), Try(NotEqual), Try(LessThanOrEqual), Try(GreaterThanOrEqual), Try(LessThan), Try(GreaterThan))
+                                                                            from rightExpr in Tok(Expression)
                                                                             select new ComparisonExpression() { Operator = compOp, Left = leftExpr, Right = rightExpr } as Expression
                                                                         ),
                                                                         Unary
@@ -160,25 +180,28 @@ namespace CloudQL.QLParser
         public static readonly Parser<char, Expression> Boolean = OneOf(
                                                                     Try(
                                                                         from leftExpr in Comparison
-                                                                        from boolOp in SkipWhitespaces.Then(OneOf(And, Or))
-                                                                        from rightExpr in SkipWhitespaces.Then(Expression)
+                                                                        from boolOp in OneOf(And, Or)
+                                                                        from rightExpr in Tok(Expression)
                                                                         select new BooleanExpression() { Left = leftExpr, Operator = boolOp, Right = rightExpr } as Expression
                                                                     ),
                                                                     Comparison
                                                                 );
         public static readonly Parser<char, Expression> Expression = Boolean;
 
-        public static readonly Parser<char, Filter> Where = from whereKeyword in String("where")
-                                                            from expr in SkipWhitespaces.Then(Expression)
+        public static readonly Parser<char, Filter> Where = from whereKeyword in Tok("where")
+                                                            from expr in Tok(Expression)
                                                             select new WhereFilter() { Expression = expr } as Filter;
+
+        //public static readonly Parser<char, Filter> Select = from selectKeyword in Tok("select")
+        //                                                     from 
 
         public static readonly Parser<char, IEnumerable<Filter>> Filters = OneOf(Where).AtLeastOnce();
 
 
         public static readonly Parser<char, Query> Query = from fromKeyword in From
-                                                           from resource in SkipWhitespaces.Then(Resource)
-                                                           from filters in SkipWhitespaces.Then(Filters.Optional())
-                                                           from selectExpr in SkipWhitespaces.Then(Select)
+                                                           from resource in Tok(Resource)
+                                                           from filters in Tok(Filters.Optional())
+                                                           from selectExpr in Tok("select")
                                                            select new Query() { };
     }
 }
