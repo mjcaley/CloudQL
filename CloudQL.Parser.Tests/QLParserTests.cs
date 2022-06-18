0b1d6cf899ec97208110a60825213716068db825
Michael Caley <mjcaley@darkarctic.com>
2022-06-18 14:17:34 -0400

    Added query parser rules

--- CloudQL.Parser.Tests/QLParserTests.cs
+++ CloudQL.Parser.Tests/QLParserTests.cs
@@ -1,3 +1,4 @@
+using CloudQL.QLParser;
 using Pidgin;
 
 namespace CloudQL.Parser.Tests
@@ -7,7 +8,7 @@ namespace CloudQL.Parser.Tests
         [Fact]
         public void ParsesResourceRecursive()
         {
-            var result = QLParser.QueryLanguage.Resource.ParseOrThrow("azure.vm");
+            var result = QueryLanguage.Resource.ParseOrThrow("azure.vm");
 
             Assert.Equal("azure", result.Name);
             Assert.NotNull(result.Child);
@@ -18,10 +19,131 @@ namespace CloudQL.Parser.Tests
         [Fact]
         public void ParsesResourceLeaf()
         {
-            var result = QLParser.QueryLanguage.Resource.ParseOrThrow("azure");
+            var result = QueryLanguage.Resource.ParseOrThrow("azure");
 
             Assert.Equal("azure", result.Name);
             Assert.Null(result.Child);
+        }
+
+        [Fact]
+        public void ParsesIdentifierExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow("ident");
+
+            var atom = Assert.IsType<IdentifierExpression>(result);
+            Assert.Equal("ident", atom.Identifier);
+        }
+
+        [Fact]
+        public void ParsesIntegerExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow("42");
+
+            var atom = Assert.IsType<IntegerExpression>(result);
+            Assert.Equal(42L, atom.Integer);
+        }
+
+        [Fact]
+        public void ParsesFloatExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow("42.42");
+
+            var atom = Assert.IsType<FloatExpression>(result);
+            Assert.Equal(42.42, atom.Float);
+        }
+
+        [Fact]
+        public void ParsesUnaryExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow("not ident");
+
+            var expr = Assert.IsType<UnaryExpression>(result);
+            Assert.Equal(UnaryOperator.Not, expr.Operator);
+        }
+
+        [Fact]
+        public void ParsesStringExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow(@"""A string value""");
+
+            var expr = Assert.IsType<StringExpression>(result);
+            Assert.Equal("A string value", expr.Value);
+        }
+
+        [Theory]
+        [InlineData("and", BooleanOperator.And)]
+        [InlineData("or", BooleanOperator.Or)]
+        public void ParsesBooleanExpression(string testInput, BooleanOperator expected)
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow($"42 {testInput} 24");
+
+            var expr = Assert.IsType<BooleanExpression>(result);
+            Assert.Equal(expected, expr.Operator);
+            var left = expr.Left as IntegerExpression;
+            Assert.Equal(42, left.Integer);
+            var right = expr.Right as IntegerExpression;
+            Assert.Equal(24, right.Integer);
+        }
+
+        [Theory]
+        [InlineData("==", ComparisonOperator.Equal)]
+        [InlineData("!=", ComparisonOperator.NotEqual)]
+        [InlineData(">", ComparisonOperator.GreaterThan)]
+        [InlineData("<", ComparisonOperator.LessThan)]
+        [InlineData(">=", ComparisonOperator.GreaterThanOrEqual)]
+        [InlineData("<=", ComparisonOperator.LessThanOrEqual)]
+        public void ParsesComparisonExpression(string testInput, ComparisonOperator expected)
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow($"42 {testInput} 24");
+
+            var expr = Assert.IsType<ComparisonExpression>(result);
+            Assert.Equal(expected, expr.Operator);
+            Assert.IsType<IntegerExpression>(expr.Left);
+            Assert.IsType<IntegerExpression>(expr.Right);
+        }
+
+        [Fact]
+        public void ParsesComplexExpression()
+        {
+            var result = QueryLanguage.Expression.ParseOrThrow("one == 1 and not 2.2");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void ParsesSelectClauseOneColumn()
+        {
+            var result = QueryLanguage.SelectClause.ParseOrThrow("select one");
+
+            var select = Assert.IsType<SelectFilter>(result);
+            Assert.Equal(new[] { "one" }, select.Columns);
+        }
+
+        [Fact]
+        public void ParsesSelectClauseMultipleColumns()
+        {
+            var result = QueryLanguage.SelectClause.ParseOrThrow("select one,two,three");
+
+            var select = Assert.IsType<SelectFilter>(result);
+            Assert.Equal(new[] { "one", "two", "three" }, select.Columns);
+        }
+
+
+        [Fact]
+        public void ParsesWhereClause()
+        {
+            var result = QueryLanguage.WhereClause.ParseOrThrow("where 1 == 1");
+
+            var where = Assert.IsType<WhereFilter>(result);
+            Assert.NotNull(where.Expression);
+        }
+
+        [Fact]
+        public void ParsesQuery()
+        {
+            var result = QueryLanguage.Query.ParseOrThrow("from azure.vm where name == 42 select");
+
+            Assert.NotNull(result);
         }
     }
 }
--- CloudQL.Parser/QLParser.cs
+++ CloudQL.Parser/QLParser.cs
@@ -15,6 +15,8 @@ namespace CloudQL.QLParser
         Select,
         From,
         Where,
+        Group,
+        Order,
     }
 
     public enum Operators
@@ -29,15 +31,121 @@ namespace CloudQL.QLParser
         public Resource? Child { get; set; }
     }
 
+    public enum BooleanOperator
+    {
+        And,
+        Or,
+    }
+
+    public enum ComparisonOperator
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        LessThan,
+        GreaterThanOrEqual,
+        LessThanOrEqual,
+    }
+
+    public enum UnaryOperator
+    {
+        Not,
+        Negative,
+        Positive,
+    }
+
+    public abstract class Expression { }
+
+    public class IntegerExpression : Expression
+    {
+        public long Integer { get; set; }
+    }
+
+    public class FloatExpression : Expression
+    {
+        public double Float { get; set; }
+    }
+
+    public class IdentifierExpression : Expression
+    {
+        public string Identifier { get; set; }
+    }
+
+    public class StringExpression : Expression
+    {
+        public string Value { get; set; }
+    }
+
+    public class BooleanExpression : Expression
+    {
+        public BooleanOperator Operator { get; set; }
+        public Expression Left { get; set; }
+        public Expression Right { get; set; }
+    }
+
+    public class ComparisonExpression : Expression
+    {
+        public ComparisonOperator Operator { get; set; }
+        public Expression Left { get; set; }
+        public Expression Right { get; set; }
+    }
+
+    public class UnaryExpression : Expression
+    {
+        public UnaryOperator Operator { get; set; }
+        public Expression Right { get; set; }
+    }
+
+    public abstract class Filter { }
+
+    public class SelectFilter : Filter
+    {
+        public IEnumerable<string> Columns { get; set; }
+    }
+
+    public class WhereFilter : Filter
+    {
+        public Expression Expression { get; set; }
+    }
+
+    public class Query
+    {
+        public Resource Resource { get; set; }
+        public IEnumerable<Filter> Filters { get; set; }
+    }
+
     public static class QueryLanguage
     {
+        private static Parser<char, T> Tok<T>(Parser<char, T> token) => Try(token).Before(SkipWhitespaces);
+        private static Parser<char, string> Tok(string token) => Tok(String(token));
+        
+        private static readonly Parser<char, BooleanOperator> And = Tok("and").ThenReturn(BooleanOperator.And);
+        private static readonly Parser<char, BooleanOperator> Or = Tok("or").ThenReturn(BooleanOperator.Or);
+        public static readonly Parser<char, BooleanOperator> BoolOps = OneOf(And, Or);
 
-        public static readonly Parser<char, Keywords> Select = String("select").ThenReturn(Keywords.Select);
-        public static readonly Parser<char, Keywords> From = String("from").ThenReturn(Keywords.From);
-        public static readonly Parser<char, Keywords> Where = String("where").ThenReturn(Keywords.Where);
+        private static readonly Parser<char, UnaryOperator> Not = Tok("not").ThenReturn(UnaryOperator.Not);
+        private static readonly Parser<char, UnaryOperator> Neg = Tok("-").ThenReturn(UnaryOperator.Negative);
+        private static readonly Parser<char, UnaryOperator> Pos = Tok("+").ThenReturn(UnaryOperator.Positive);
+        public static readonly Parser<char, UnaryOperator> UnaryOps = OneOf(Not, Neg, Pos);
 
-        public static readonly Parser<char, Operators> Dot = Char('.').ThenReturn(Operators.Dot);
-        public static readonly Parser<char, Operators> Comma = Char(',').ThenReturn(Operators.Comma);
+        private static readonly Parser<char, ComparisonOperator> Equal = from _ in Tok("==")
+                                                                        select ComparisonOperator.Equal;
+        private static readonly Parser<char, ComparisonOperator> NotEqual = from _ in Tok("!=")
+                                                                           select ComparisonOperator.NotEqual;
+        private static readonly Parser<char, ComparisonOperator> LessThan = from _ in Tok("<")
+                                                                           select ComparisonOperator.LessThan;
+        private static readonly Parser<char, ComparisonOperator> GreaterThan = from _ in Tok(">")
+                                                                              select ComparisonOperator.GreaterThan;
+        private static readonly Parser<char, ComparisonOperator> LessThanOrEqual = from _ in Tok("<=")
+                                                                                  select ComparisonOperator.LessThanOrEqual;
+        private static readonly Parser<char, ComparisonOperator> GreaterThanOrEqual = from _ in Tok(">=")
+                                                                                     select ComparisonOperator.GreaterThanOrEqual;
+        public static readonly Parser<char, ComparisonOperator> CompOps = OneOf(
+            Try(Equal), Try(NotEqual), Try(GreaterThanOrEqual), Try(LessThanOrEqual), Try(GreaterThan), Try(LessThan)
+            );
+         
+        public static readonly Parser<char, Operators> Dot = Tok(".").ThenReturn(Operators.Dot);
+        public static readonly Parser<char, Operators> Comma = Tok(",").ThenReturn(Operators.Comma);
 
         public static readonly Parser<char, string> Identifier = from first in Letter
                                                                  from rest in OneOf(LetterOrDigit, Char('_')).ManyString()
@@ -46,5 +154,75 @@ namespace CloudQL.QLParser
         public static readonly Parser<char, Resource> Resource = from ident in Identifier
                                                                  from child in Rec(() => Dot.Then(Resource)).Optional()
                                                                  select new Resource() { Name = ident, Child = child.GetValueOrDefault(() => null) };
+
+        public static readonly Parser<char, Expression> IdentifierAtom = from ident in Tok(Identifier)
+                                                                         select new IdentifierExpression() { Identifier = ident } as Expression;
+        public static readonly Parser<char, Expression> IntegerAtom = from integer in Tok(LongNum)
+                                                                      select new IntegerExpression() { Integer = integer } as Expression;
+        public static readonly Parser<char, Expression> StringAtom =
+            AnyCharExcept('"').ManyString().Between(Char('"')).Map(s => new StringExpression() { Value = s } as Expression);
+
+        private static readonly Parser<char, string> FloatFirst = OneOf(
+            String("0"),
+            Digit.Where(c => c != '0').Then(Digit.ManyString())
+            );
+        public static readonly Parser<char, double> FloatLiteral = FloatFirst.Then(Char('.')).Then(Digit.Many()).MapWithInput(
+            (span, _) =>
+            {
+                var success = double.TryParse(span, out var result);
+                if (success)
+                {
+                    return (double?)result;
+                }
+                return null;
+            }
+            )
+            .Assert(result => result.HasValue)
+            .Select(result => result!.Value);
+                                                                   
+        public static readonly Parser<char, Expression> FloatAtom = from floatAtom in Tok(FloatLiteral)
+                                                                    select new FloatExpression() { Float = floatAtom } as Expression;
+        public static readonly Parser<char, Expression> Atom = OneOf(IdentifierAtom, Try(FloatAtom), Try(IntegerAtom), StringAtom);
+        public static readonly Parser<char, Expression> Unary = OneOf(
+                                                                    Try(
+                                                                        from unaryOp in UnaryOps
+                                                                        from expr in Tok(Expression)
+                                                                        select new UnaryExpression() { Operator = unaryOp, Right = expr } as Expression
+                                                                    ),
+                                                                    Atom);
+        public static readonly Parser<char, Expression> Comparison = OneOf(
+                                                                        Try(
+                                                                            from leftExpr in Unary
+                                                                            from compOp in CompOps
+                                                                            from rightExpr in Tok(Expression)
+                                                                            select new ComparisonExpression() { Operator = compOp, Left = leftExpr, Right = rightExpr } as Expression
+                                                                        ),
+                                                                        Unary
+                                                                        );
+        public static readonly Parser<char, Expression> Boolean = OneOf(
+                                                                    Try(
+                                                                        from leftExpr in Comparison
+                                                                        from boolOp in BoolOps
+                                                                        from rightExpr in Tok(Expression)
+                                                                        select new BooleanExpression() { Left = leftExpr, Operator = boolOp, Right = rightExpr } as Expression
+                                                                    ),
+                                                                    Comparison
+                                                                );
+        public static readonly Parser<char, Expression> Expression = Boolean;
+
+        public static readonly Parser<char, Filter> WhereClause = from whereKeyword in Tok("where")
+                                                                  from expr in Tok(Expression)
+                                                                  select new WhereFilter() { Expression = expr } as Filter;
+
+        public static readonly Parser<char, Filter> SelectClause = from selectKeyword in Tok("select")
+                                                                   from columns in Identifier.Separated(Comma)
+                                                                   select new SelectFilter() { Columns = columns } as Filter;
+
+        public static readonly Parser<char, IEnumerable<Filter>> Filters = OneOf(SelectClause, WhereClause).Many();
+
+        public static readonly Parser<char, Query> Query = from fromKeyword in Tok("from")
+                                                           from resource in Tok(Resource)
+                                                           from filters in Filters
+                                                           select new Query() { Resource = resource, Filters = filters };
     }
 }
